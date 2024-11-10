ed0713caff0d556d729b970a99b7ad8e9978c7fc
Powerstar0 <133736954+Powerstar0@users.noreply.github.com>
2024-11-10 02:41:46 -0600

    Initial commit

--- 291project.py
+++ 291project.py
@@ -1,4 +1,8 @@
-with open("test.txt", newline='') as file:
-    lines = file.readlines()
-    for line in lines:
-        print(lines)
+from IPython.display import display, HTML
+
+# Read the HTML file content
+with open("C:/Users/Johnny/CS291-Project/map_with_airports_edges_with_labels.html", "r") as file:
+    html_content = file.read()
+
+# Display the HTML content directly
+display(HTML(html_content))
